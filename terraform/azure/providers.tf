28890727b7188384a4cec0ca63e5222706ff0a35
Anand Kumar Singh <anand1911991@outlook.com>
2025-12-29 13:56:11 +0100

    updated azure providers tf

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.34.0"
+      version = ">=4.54.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~>2.4.0"
+      version = "~>2.7.0"
     }
     random = {
       source  = "hashicorp/random"
